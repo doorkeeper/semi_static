5c9e2c9bebe28f752f5c983e83e067b57a54e0b6
Paul McMahon <paul@tokyodev.com>
2021-02-03 10:22:30 +0900

    Set rails environment variable

--- spec/features/acceptance_helper.rb
+++ spec/features/acceptance_helper.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path(File.dirname(__FILE__) + "/fake/app")
 require 'capybara/rspec'
 
